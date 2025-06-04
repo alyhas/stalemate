c866b026a170b432fdaf916b371ea5b30765d0a2
alyhas <64784836+alyhas@users.noreply.github.com>
2025-06-04 18:30:38 +0400

    Add fade animation to logger context menu

--- src/components/logger/ContextMenu.tsx
+++ src/components/logger/ContextMenu.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./context-menu.scss";
 
 export type ContextMenuItem = {
@@ -17,21 +17,32 @@ export default function ContextMenu({
   items: ContextMenuItem[];
   onClose: () => void;
 }) {
+  const [closing, setClosing] = useState(false);
+
+  const handleClose = () => {
+    setClosing(true);
+  };
+
   useEffect(() => {
-    const handler = () => onClose();
+    const handler = () => handleClose();
     window.addEventListener("click", handler);
     return () => window.removeEventListener("click", handler);
-  }, [onClose]);
+  }, []);
 
   return (
-    <ul className="context-menu" style={{ top: y, left: x }} role="menu">
+    <ul
+      className={`context-menu${closing ? " closing" : ""}`}
+      style={{ top: y, left: x }}
+      role="menu"
+      onAnimationEnd={() => closing && onClose()}
+    >
       {items.map((item) => (
         <li key={item.label} role="none">
           <button
             role="menuitem"
             onClick={() => {
               item.onClick();
-              onClose();
+              handleClose();
             }}
           >
             {item.label}
